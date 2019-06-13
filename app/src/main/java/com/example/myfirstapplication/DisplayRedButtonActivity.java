a1c56134fed18cc220f7c97780812cf5b690741f
jbrownrs <36203116+jbrownrs@users.noreply.github.com>
2019-06-13 18:12:45 +0100

    Adds a following button to take to another page

---
+++ app/src/main/java/com/example/myfirstapplication/DisplayRedButtonActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfirstapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DisplayRedButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_red_button);
+    }
+}
